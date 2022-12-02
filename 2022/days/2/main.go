ee72b7f0456e6c11e49c35ef5e97a043b9016325
ofsho <ofekbendavid9@gmail.com>
2022-12-02 15:52:30 +0200

    improved solution

--- 2022/days/2/main.go
+++ 2022/days/2/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -17,76 +16,16 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	points := 0
-	tscore := 0
+	tpoints := 0
 	for scanner.Scan() {
-		match := strings.Split(scanner.Text(), " ")
-		points += (ar(ag(match[1])-ag(match[0])) + ag(match[1]))
+		match := scanner.Text()
+		points += int(1 + (rune(match[2]) - 'X') + (4+
+			(rune(match[2])-'X')-(rune(match[0])-'A'))%3*3)
 
-		tscore += (rval(match[1]) + part2(match[0], match[1]))
-		fmt.Println(part2("A", "X"))
+		tpoints += int(1 + (rune(match[2])-'X')*3 + (2+
+			(rune(match[2])-'X')+(rune(match[0])-'A'))%3)
 	}
 
 	fmt.Println(points)
-	fmt.Println(tscore)
-}
-
-func ar(i int) int {
-	if i == 1 || i == -2 {
-		return 6
-	} else if i == 0 {
-		return 3
-	} else {
-		return 0
-	}
-}
-
-func ag(str string) int {
-	if str == "A" || str == "X" {
-		return 1
-	} else if str == "B" || str == "Y" {
-		return 2
-	} else if str == "C" || str == "Z" {
-		return 3
-	}
-	return 0
-}
-
-func rval(i string) int {
-	if i == "X" {
-		return 0
-	} else if i == "Y" {
-		return 3
-	} else {
-		return 6
-	}
-}
-
-func part2(i, b string) int {
-	switch b {
-	case "X":
-		n := 0
-		for range []int{1, 2, 3} {
-			n += 1
-			if ar(n-ag(i)) == 0 {
-				return n
-			}
-		}
-	case "Y":
-		n := 0
-		for range []int{1, 2, 3} {
-			n += 1
-			if ar(n-ag(i)) == 3 {
-				return n
-			}
-		}
-	case "Z":
-		n := 0
-		for range []int{1, 2, 3} {
-			n += 1
-			if ar(n-ag(i)) == 6 {
-				return n
-			}
-		}
-	}
-	return 0
+	fmt.Println(tpoints)
 }
